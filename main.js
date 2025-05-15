af29e5b81d88f905b889e67a6afb6f04d36859cc
NANDA <fernandaalmeidacarloto@gmail.com>
2025-05-15 07:49:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -23,6 +23,9 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 
+for (let i=0; i<contadores.length;i++){
+   contadores[i].textContent = calculaTempo(tempos[i]);
+}
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
